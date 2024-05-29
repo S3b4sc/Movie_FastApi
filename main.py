3e07ea390c8f42a68b1185f5ed968a7cc644a4c3
Sebastian Carrillo Mejia <sebastian.carrillo1@udea.edu.co>
2024-05-28 22:20:35 -0500

    Updated .gitignore, also in main.py, added delete, put methods, some by defect squemas, routes for updating, deleting and adding movies, and set by defect status codes, and implementing data validations for all parameters and datatypes.

--- main.py
+++ main.py
@@ -1,7 +1,99 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+
+from pydantic import BaseModel, Field
+
+from typing import Optional, List
+
 
 app = FastAPI()
+app.title = 'My project using FastApi'
+app.version = '0.0.1'
 
-@app.get('/')
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title:str = Field( min_length=5 , max_length=15)
+    overview:str = Field( min_length=15 , max_length=50 )
+    year:int =  Field( le=2022 )
+    rating:float = Field(ge=1, le=10.0)
+    category:str = Field(min_length=5 , max_length=15)
+    
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+               {
+                'id': 1,
+                'title' : 'Crepusculo',
+                'overview' : 'The twilight is almost better than sunday',
+                'year' : '2022',
+                'rating' : 9.5,
+                'category' : 'Phantasy'
+            }
+            ]
+        }
+    }
+
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar 2",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2015",
+        "rating": 8.0,
+        "category": "Acción"
+    }
+]
+
+@app.get('/', tags=['home'])
 def message():
-    return 'hello world!'
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'], response_model= List[Movie], status_code=200)
+def get_movies()-> List[Movie]:
+    return JSONResponse(status_code=200, content=movies)
+
+@app.get('/movies/{id}', tags=['movies'], response_model= Movie)
+def get_movie(id:int = Path(ge=1, le=2000)):
+    for item in movies:
+        if item['id'] == id:
+            return JSONResponse(content=item)
+    return JSONResponse(status_code=404, content=[])
+
+@app.get('/movies/', tags=['movies'], response_model= List[Movie])
+def get_movies_by_category(category:str = Query(min_length=5, max_length=15)) -> List[Movie]:
+    result = list(filter( lambda movie: movie['category'] == category, movies ))
+    return JSONResponse(content=result)
+
+@app.post('/movies', tags=['movies'], response_model= dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(status_code=201,content={'Message':'The movie has been registered.'})
+
+@app.put('/movies/{id}', tags=['movies'], response_model= dict,  status_code=200)
+def update_movie(id:int, movie: Movie) -> dict:
+    for item in movies:
+        if item['id'] == id:
+            item.update({
+            'title': movie.title,
+            'overview': movie.overview,
+            'year': movie.year,
+            'rating': movie.rating,
+            'category': movie.category
+            })
+    return JSONResponse( status_code=200, content={'Message':'The movie has been correctly updated.'})
+
+@app.delete('/movies/{id}', tags=['movies'], response_model= dict,  status_code=200)
+def delete_movie(id:int) ->dict:
+    for item in movies:
+        if item['id'] == id:
+            movies.remove(item)
+        return JSONResponse( status_code=200, content={'Message':'The movie has been correctly deleted.'})
